a3f5271d8ce8b544bdaabbfdd022f2da9d0be72f
Pallavi <pallavi25kishore@gmail.com>
2024-05-27 12:53:31 -0700

    add axios.post for when a user enters a github username into text field

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -5,11 +5,22 @@ const Search = ({ onSearch }) => {
   const[term, setTerm] = useState('')
 
   const onChange = (e) => {
+    event.preventDefault();
     setTerm(e.target.value);
   }
 
   const search = () => {
-    onSearch(term);
+    //onSearch(term);
+    event.preventDefault();
+    axios.post('/repose', {reponame: term})
+    .then((response) => {
+       console.log('posted', response);
+       //axios.get() // fill in
+    });
+    .catch((error) => {
+      console.log(error);
+    });
+    setTerm('');
   }
 
   return (
